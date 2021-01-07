aaa6bc006b339bb606623b3f8cce77463a0f8877
jonec76 <jonec9676@gmail.com>
2021-01-07 16:43:12 +0800

    Improve the memory allocating in leetcode 1483.
    Using depth table for allocating exact size
    
    Runtime: 68.42%
    Memory: 100%

--- 1483.c
+++ 1483.c
@@ -2,8 +2,9 @@
 #include<stdlib.h>
 
 typedef struct {
+    int *depth;
     int **parent;
-    int max_level;
+    int max_depth;
     int n;
 } TreeAncestor;
 
@@ -12,12 +13,16 @@ TreeAncestor *treeAncestorCreate(int n, int *parent, int parentSize)
     TreeAncestor *obj = malloc(sizeof(TreeAncestor));
     obj->parent = malloc(n * sizeof(int *));
     obj->n = n;
-    int max_level = 32 - __builtin_clz(n);
-
+    obj->depth = malloc(n * sizeof(int));
+    int max_depth = -1;
+    memset(obj->depth, 0, n * sizeof(int));
     obj->parent[0] = malloc(sizeof(int));
 
     for (int i = 1; i < n; i++) {
-        int node_level = 32 - __builtin_clz(i);
+        obj->depth[i] = obj->depth[parent[i]]+1;
+        if(obj->depth[i] > max_depth)
+            max_depth = obj->depth[i];
+        int node_level = 32 - __builtin_clz(obj->depth[i]);
         obj->parent[i] = malloc(node_level * sizeof(int));
         for (int j = 0; j < node_level; j++)
             obj->parent[i][j] = -1;
@@ -25,38 +30,33 @@ TreeAncestor *treeAncestorCreate(int n, int *parent, int parentSize)
     for (int i = 0; i < parentSize; i++)
         obj->parent[i][0] = parent[i];
 
-    int level_counter = 1;
-    while(level_counter < max_level){
-        int start_idx = (1 << level_counter);
+    int bit_idx = 1;
+    int max_bit = 32 - __builtin_clz(max_depth) + 1;
+    while(bit_idx < max_bit){
+        int start_idx = (1 << bit_idx);
         for(int j=start_idx;j<parentSize;j++){
-            int parent_level = (obj->parent[j][level_counter - 1] == 0)? 1
-                            :32 - __builtin_clz(obj->parent[j][level_counter - 1]);
-            if((level_counter > parent_level) || (obj->parent[j][level_counter - 1]==-1))
-                obj->parent[j][level_counter] = -1;
+            if(32 - __builtin_clz(obj->depth[j]) <= bit_idx)
+                continue;
+            if(obj->parent[j][bit_idx - 1]==-1)
+                obj->parent[j][bit_idx] = -1;
             else
-                obj->parent[j][level_counter] = obj->parent[obj->parent[j][level_counter - 1]][level_counter - 1];
+                obj->parent[j][bit_idx] = obj->parent[obj->parent[j][bit_idx - 1]][bit_idx - 1];
         }
-        level_counter++;
+        bit_idx++;
     }
-    obj->max_level = max_level;
+    obj->max_depth = max_depth;
     return obj;
 }
 
 int treeAncestorGetKthAncestor(TreeAncestor *obj, int node, int k)
 {
-    if(node < k)
+    int max_depth = obj->max_depth;
+    if(obj->depth[node] < k)
         return -1;
-    int max_level = obj->max_level;
-    for (int i = 0; i < max_level && node != -1; ++i){
-        int parent_level = (node == 0)? 1
-                            :32 - __builtin_clz(node);
-
-        if ((k & (1 << i))){
-            if(parent_level <= i)
-                return -1;
+    
+    for (int i = 0; i < (32 - __builtin_clz(max_depth)) && node != -1; ++i){
+        if ((k & (1 << i)))
             node = obj->parent[node][i];
-
-        }
     }
     return node;
 }
@@ -66,6 +66,7 @@ void treeAncestorFree(TreeAncestor *obj)
     for (int i = 0; i < obj->n; i++)
         free(obj->parent[i]);
     free(obj->parent);
+    free(obj->depth);
     free(obj);
 }
 
@@ -90,7 +91,18 @@ int main(){
     int n=10, parentSize=n;
     int parent[] = {-1,0,0,1,2,0,1,3,6,1};
     TreeAncestor* obj = treeAncestorCreate(n, parent, parentSize);
-    int param_1 = treeAncestorGetKthAncestor(obj, 9, 7);
+
+    for(int i=0;i<n;i++){
+        int node_level = 32 - __builtin_clz(obj->depth[i]);
+        for(int j=0;j<node_level;j++){
+            printf("%d ", obj->parent[i][j]);
+        }
+        puts("\n");
+    }
+
+
+
+    int param_1 = treeAncestorGetKthAncestor(obj, 7, 4);
     printf("\nans: %d", param_1);
     treeAncestorFree(obj);
 }
